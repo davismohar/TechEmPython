0bb13e1f4b08200b7421248154c479f30439efa1
davismohar <davismohar@gmail.com>
2016-07-10 00:31:55 -0400

    Initial Commit

---
+++ loops.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    print i
+
+x = 0
+while x < 1:
+    print "0 < 1"
---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle
+turtle1 = turtle.Turtle()
+
+turtle1.forward(100)
+turtle1.left(90)
+turtle1.forward(100)
+turtle1.left(90)
+turtle1.forward(100)
+turtle.left(90)
+turtle1.forward(100)
